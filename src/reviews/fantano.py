05a162261d4d0e527bae84cfcecc7ef3a9772e46
Jeff Brennan <jeffbrennan10@gmail.com>
2022-08-03 18:12:02 -0400

    add vid transcripts to replace vid descriptions

--- src/reviews/fantano.py
+++ src/reviews/fantano.py
@@ -1,23 +1,18 @@
-# -*- coding: utf-8 -*-
-
-# Sample Python code for youtube.channels.list
-# See instructions for running these code samples locally:
-# https://developers.google.com/explorer-help/code-samples#python
-
 import os
-
 import google_auth_oauthlib.flow
 import googleapiclient.discovery
 import googleapiclient.errors
 import pandas as pd
+import concurrent.futures
+
+from yt_dlp import YoutubeDL
 
 os.chdir('C:/Users/jeffb/Desktop/Life/personal-projects/spotify')
 scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
-# CHANNEL_ID = 'UCt7fwAhXDy3oNFTAzF2o8Pw'
+
 CLIENT_SECRET = "backend/youtube_secret.json"
 CHANNEL_UPLOADS = 'UUt7fwAhXDy3oNFTAzF2o8Pw'
-EXISTING_IDS = pd.read_csv('reviews/fantano.tsv', sep='\t').squeeze()['vid_id']
-
+EXISTING_IDS = pd.read_csv('reviews/fantano.csv', sep='\t').squeeze()['vid_id'].to_list()
 
 def build_client():
     os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
@@ -35,11 +30,24 @@ def build_client():
     return youtube
 
 
-def parse_item(item): 
-    results = {'vid_id': item['id'],
+def download_caption(video_ids):
+    ydl_opts = {
+        "skip_download": True,
+        "writeautomaticsub": True,
+        "subtitleslangs": ["en"],
+        'outtmpl': 'reviews/fantano_subs/%(id)s.%(ext)s',
+        "subtitlesformat": "json3"
+    }
+    with YoutubeDL(ydl_opts) as ydl:
+        ydl.download(video_ids)
+
+
+def parse_item(item):
+    video_id = item['snippet']['resourceId']['videoId']
+
+    results = {'vid_id': video_id,
                'vid_published': item['snippet']['publishedAt'],
-               'vid_title': item['snippet']['title'],
-               'vid_description': item['snippet']['description']
+               'vid_title': item['snippet']['title']
                }
     return results
 
@@ -54,11 +62,9 @@ def get_vids(youtube, next_page=''):
     )
     response = request.execute()
 
-    vid_ids = [i['id'] for i in response['items']]
+    vid_ids = [i['snippet']['resourceId']['videoId'] for i in response['items']]
     new_ids = set(vid_ids) - set(EXISTING_IDS)
     
-
-    # TODO: get positions of actual new ids and run using that instead
     if len(new_ids) > 0: 
         output = {'NextPageToken': response['nextPageToken'],
                   'items': response['items'][0:len(new_ids)]
@@ -88,9 +94,15 @@ def main():
                 print('All videos parsed')
                 break
 
-    all_vids = pd.DataFrame(results)
-    all_vids.to_csv('reviews/fantano.csv', sep='\t', mode='a', index=False, header=False)
-    # all_vids.to_csv('reviews/fantano.csv', sep='\t', index=False)
+        all_vids = pd.DataFrame(results)
+        all_vids.to_csv('reviews/fantano.csv', sep='\t', mode='a', index=False, header=False)
+
+        all_ids = EXISTING_IDS.extend(all_vids['vid_id'].squeeze().to_list())
+        existing_captions = [x.split('.')[0] for x in os.listdir('reviews/fantano_subs')]
+        captions_to_dl = list(set(all_ids) - set(existing_captions))
+
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            executor.map(download_caption, captions_to_dl)
 
 
 if __name__ == "__main__":
